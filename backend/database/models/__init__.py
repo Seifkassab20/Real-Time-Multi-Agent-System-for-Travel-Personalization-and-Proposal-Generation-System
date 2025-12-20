668ff679af650c09347e4a17c48a2914c29f97c7
maryam <maryamabdallahshalash@gmail.com>
2025-12-20 22:25:51 +0200

    Add models for Customer, ServiceAgent, Calls, and ItineraryDB; update __init__.py for model imports

--- backend/database/models/__init__.py
+++ backend/database/models/__init__.py
@@ -1,2 +1,6 @@
 from .customer_profile import CustomerProfileDB
-__all__ = ['CustomerProfileDB']
+from .customers import Customer
+from .service_agents import ServiceAgent
+from .calls import Calls
+
+__all__ = ['CustomerProfileDB', 'Customer', 'ServiceAgent', 'Calls']
